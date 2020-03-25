854a7c2fd70e53d03000765c32d8e47a72a8d271
adoble <andrew.l.doble@gmail.com>
2020-03-25 13:04:44 +0100

    Changed to JUnit 5

--- src/test/java/templato/LibraryTest.java
+++ src/test/java/templato/LibraryTest.java
@@ -3,12 +3,19 @@
  */
 package templato;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+
+
 
 public class LibraryTest {
-    @Test public void testSomeLibraryMethod() {
+    
+	@Test 
+	public void testSomeLibraryMethod() {
         Library classUnderTest = new Library();
-        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+        assertTrue(classUnderTest.someLibraryMethod(), "someLibraryMethod should return 'true'");
+     
+
     }
 }
