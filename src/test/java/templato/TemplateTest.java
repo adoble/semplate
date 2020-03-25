2941a5e0a7d1f09417be6ede41ab5cd25922118c
adoble <andrew.l.doble@gmail.com>
2020-03-25 16:41:36 +0100

    Test case stub

---
+++ src/test/java/templato/TemplateTest.java
@@ -0,0 +1,22 @@
+package templato;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+
+class TemplateTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
