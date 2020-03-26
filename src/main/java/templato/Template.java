4c766e37b9d7333e826a9017100caf404af2b718
adoble <andrew.l.doble@gmail.com>
2020-03-26 16:39:17 +0100

    Commented out redundant dependencies

--- src/main/java/templato/Template.java
+++ src/main/java/templato/Template.java
@@ -3,8 +3,12 @@
  */
 package templato;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.stream.Stream;
 
 /**
  * TODO
@@ -12,32 +16,44 @@ import java.nio.file.Path;
  *
  */
 public class Template  {
-	private Path templateFile;
+	// Special fields are preceded with template
+	final private String templateCommentField = "${template.comment}";
+	
+	private Path templatePath;
+	String commentStartDelimiter;
+	String commentEndDelimiter;
 
 	/**
 	 * TODO
-	 * @param templateFile
+	 * @param templatePath
 	 */
 	public Template() {
 		super();
 	} 
 
-	/**
-	 * TODO
-	 * @param templateFile
-	 */
-	public Template(Path templateFile) {
-		super();
-		this.templateFile = templateFile;
-	}
 	
 	/**
 	 * TODO
 	 * @param templatePath
 	 */
-	public void config(Path templatePath) {
-		this.templateFile = templatePath;
+	public void config(Path templatePath) throws IOException {
+		this.templatePath = templatePath;
+	
+		try (Stream<String> stream = Files.lines(templatePath, Charset.defaultCharset())) {
+
+			String templateComment = stream.filter(line -> line.contains(templateCommentField)).findAny().orElse("");
+			if (!templateComment.isEmpty()) {
+				commentStartDelimiter = templateComment.substring(0,templateComment.indexOf(templateCommentField));
+				commentEndDelimiter = templateComment.substring(commentStartDelimiter.length() + templateCommentField.length(), templateComment.length());
+				
+			}
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+			
+
 	}
+	
 	
 	/**
 	 * TODO
@@ -45,15 +61,30 @@ public class Template  {
 	 */
 	public void config(InputStream templateStream) { 
 		//TODO
+		
+		/*
+		 try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+			
+			//Get Stream with lines from BufferedReader
+			reader.lines()
+			//Gives each line as string to the changeTrumpToDrumpf method of this.
+			.map(this::changeTrumptoDrumpf)
+			//Calls for each line the print method of this.
+			.forEach(this::print);
+			
+		} catch (IOException ioException) {
+			ioException.printStackTrace();
+		}
+		 */
 	}
 	
 	/**
 	 * Generates a markdown file as specified by the template file using the information
 	 * in the node object
 	 * @param object An object annotated with template field information
-	 * @param outputFile
+	 * @param outputFilePath
 	 */
-	public void generate(Object object, Path outputFile) {
+	public void generate(Object object, Path outputFilePath) {
 		//TODO 
 	}
 	
@@ -61,9 +92,9 @@ public class Template  {
 	 * Updates a markdown files using the annotated fields in the object .
 	 * 
 	 * @param object
-	 * @param outputFile
+	 * @param outputFilePath
 	 */
-	public void update(Object object, Path outputFile) {
+	public void update(Object object, Path outputFilePath) {
 		//TODO 
 	}
 	
@@ -72,12 +103,27 @@ public class Template  {
 	 * Reads the specified markup file and creates a new  object of class nodeClass that contains the
 	 * data semantically represented in the file. 
 	 * @param objectClass
-	 * @param markupFile
+	 * @param markupFilePath
 	 * @return
 	 */
-	public Object read(Class<Object> objectClass, Path markupFile) {
+	public Object read(Class<Object> objectClass, Path markupFilePath) {
 		//TODO 
 		return null;
 	}
+
+	public Path getTemplatePath() {
+		
+		return templatePath;
+	}
+
+	public String getCommentStartDelimiter() {
+		return commentStartDelimiter;
+	}
+
+	public String getCommentEndDelimiter() {
+		return commentEndDelimiter;
+	}
+	
+	
 	
 }
--- src/test/java/templato/TemplateTest.java
+++ src/test/java/templato/TemplateTest.java
@@ -2,21 +2,86 @@ package templato;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.FileSystem;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import org.junit.jupiter.api.*;
 
+import com.google.common.jimfs.Configuration;
+import com.google.common.jimfs.Jimfs;
+
 class TemplateTest {
+	final static String templateFileName = "simple_template.md";
+	
+	private static FileSystem fileSystem;
+	private Path rootPath;
+	private Path templatesPath;
+	
+	private Path templateFile;
+	
 
 	@BeforeEach
 	void setUp() throws Exception {
+		// Set up the mock file system
+		fileSystem = Jimfs.newFileSystem(Configuration.unix());
+
+		rootPath = fileSystem.getPath("/files");  // Test files here
+		Files.createDirectory(rootPath);
+
+		templatesPath = fileSystem.getPath("/templates");  // Template files here
+		Files.createDirectory(templatesPath);
+		
+		//Copy a test template from the resources to the mock file system
+		templateFile = templatesPath.resolve(templateFileName);
+		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+		InputStream in  = classLoader.getResourceAsStream(templateFileName);
+		Files.copy(in, templateFile);
+		assertTrue(Files.exists(templateFile));
+		
+		
+
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
 	}
 
+
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testConfigWithPath() {
+		Template t = new Template();
+		assertNotNull(t);
+		
+		try {
+			t.config(templateFile);
+		} catch (IOException e) {
+			fail("Unxpected exception: " + e.getMessage());
+		}
+		
+		assertTrue(t.getTemplatePath().toString().equals("/templates/" + templateFileName));
+		
+		assertTrue(t.getCommentStartDelimiter().equals("<!--"));
+		assertTrue(t.getCommentEndDelimiter().equals("-->"));
+		
+	
+	}
+	
+	
+	@Test
+	void testConfigWithStream() {
+		Template t = new Template();
+		assertNotNull(t);
+		
+		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+		InputStream in = classLoader.getResourceAsStream(templateFileName);
+		t.config(in);
+
+		assertTrue(t.getCommentStartDelimiter().equals("<!--"));
+		assertTrue(t.getCommentEndDelimiter().equals("-->"));
+
 	}
 
 }
