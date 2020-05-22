c3d735e908412c6f16c45754cdbac84415ddd292
Andrew Doble <andrew.l.doble@gmail.com>
2020-05-22 15:45:47 +0200

    Extended with exception message

--- src/main/java/templato/ReadException.java
+++ src/main/java/templato/ReadException.java
@@ -2,4 +2,14 @@ package templato;
 
 public class ReadException extends Exception {
 
+	private static final long serialVersionUID = -7355467559376185515L;
+
+	public ReadException() {
+		super(); 
+	}
+
+	public ReadException(String message) {
+		super(message);
+	}
+
 }
