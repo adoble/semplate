5b10837f1a3522fd2875e27c35fdf8dd00c9f8fb
Andrew Doble <adoble@users.noreply.github.com>
2021-03-10 17:37:32 +0100

    Added method to facilitate stream collecting

--- semplate-lib/src/main/java/semplate/Delimiters.java
+++ semplate-lib/src/main/java/semplate/Delimiters.java
@@ -6,55 +6,36 @@ import java.util.*;
 
 
 
-public class Delimiters implements Iterable<Delimiters.Delimiter>{
-	
-	public static class Delimiter {
-		private Optional<String> start = Optional.empty();
-		private Optional<String> end = Optional.empty();
-		
-		Delimiter start(String startDelimiter) {
-			this.start = filter(startDelimiter);
-			return this;
-		}
-		
-		Delimiter end(String endDelimiter) {
-			this.end = filter(endDelimiter);
-			return this;
-		}
-		
-		private Optional<String> filter(String delimiterString) {
-			return Optional.ofNullable(delimiterString).filter(s -> s.length() > 0);
-		}
-		
-		
-		Optional<String> start() { return start; }
-		Optional<String> end() { return end; }
-		
-		
-	}
-    
+public class Delimiters implements Iterable<Delimiter>{
 	private Delimiter comment = new Delimiter();
 	
 	private ArrayList<Delimiter> delimiters = new ArrayList<>();
 	
-	public Delimiters() {
-	  //TODO
-	}
-
-    
 	public void add(String startDelimiter, String endDelimiter) {
 		Delimiter delimiter = (new Delimiter()).start(startDelimiter).end(endDelimiter);
-		delimiters.add(delimiter);
+		add(delimiter);
 	}
 	
-    public void addPair(String delimiterPair) {
-	checkArgument(delimiterPair.length() == 2, "Calling addPair with %s. Only2 charaters are allowed.", delimiterPair);
+	public void addPair(String delimiterPair) {
+		checkArgument(delimiterPair.length() == 2, "Calling addPair with %s. Only 2 characters are allowed.", delimiterPair);
+
+		add((new Delimiter()).pair(delimiterPair));
+	}
 	
-	Delimiter delimiter = (new Delimiter()).start(delimiterPair.substring(0, 1)).end(delimiterPair.substring(1));
-	delimiters.add(delimiter);
+	public void add(Delimiter delimiter) {
+		delimiters.add(delimiter);
+		
+	}
 	
-	
-}
+	/**Appends all of the specified delimiters to the end of this list, in the order 
+	 * that they are returned by the specified delimiters Iterator.
+	 */
+	public void add(Delimiters delimiters) {
+	  	for (Delimiter delim: delimiters) {
+	  		this.add(delim);
+	  	}
+	}
+    
 	public int number() {
 		return delimiters.size();
 	}
@@ -89,6 +70,24 @@ public class Delimiters implements Iterable<Delimiters.Delimiter>{
 	@Override
 	public Iterator<Delimiter> iterator() {
 		return delimiters.iterator();
+	}
+
+	/* 
+	 * Is the string s surrounded by one of the specified delimiters
+	 * @param s The string to be tested
+	 */
+	public boolean suround(String s) {
+		if (s.length() < 2) return false;
+		
+		Optional<String> startChar  = Optional.of(s.substring(0, 1));
+		Optional<String> endChar  = Optional.of(s.substring(s.length() - 1));
+		
+		for (Delimiter delim: this) {
+			if (startChar.equals(delim.start()) && endChar.equals(delim.end())) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 
