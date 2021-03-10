5fdf43918e1036e053e66d1ba6f996c5bbda3f4c
Andrew Doble <adoble@users.noreply.github.com>
2021-03-10 17:39:29 +0100

    Extracted Delimiter class from Delimiters

---
+++ semplate-lib/src/main/java/semplate/Delimiter.java
@@ -0,0 +1,42 @@
+package semplate;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
+import java.util.Optional;
+
+/** TODO
+ * @author Andrew Doble
+ *
+ */
+public class Delimiter {
+    private Optional<String> start = Optional.empty();
+	private Optional<String> end = Optional.empty();
+
+	Delimiter start(String startDelimiter) {
+		this.start = filter(startDelimiter);
+		return this;
+	}
+
+	Delimiter end(String endDelimiter) {
+		this.end = filter(endDelimiter);
+		return this;
+	}
+
+	private Optional<String> filter(String delimiterString) {
+		return Optional.ofNullable(delimiterString).filter(s -> s.length() > 0);
+	}
+
+
+	Optional<String> start() { return start; }
+	Optional<String> end() { return end; }
+	
+	Delimiter pair(String delimiterPair) {
+		checkArgument(delimiterPair.length() == 2, "Only 2 characters are allowed in a delimiter pair - instead %s was specified.", delimiterPair);
+		return this.start(delimiterPair.substring(0, 1)).end(delimiterPair.substring(1));
+	}
+
+	@Override
+	public String toString() { return "Delimiter [start=" + start + ", end=" + end + "]"; }
+
+
+}
