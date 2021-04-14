31e6d21aae3072970577bdb68487c52b301f0685
Andrew Doble <adoble@users.noreply.github.com>
2021-04-14 18:28:11 +0200

    Updated comments

--- semplate-lib/src/main/java/semplate/Block.java
+++ semplate-lib/src/main/java/semplate/Block.java
@@ -22,7 +22,11 @@ class Block {    StringBuffer text = new StringBuffer();
 	}
 		
 	/** 
+	 * Initialise the block using the semantic block line in the markdown 
 	 * 
+	 * Preconditions: 
+	 * - Parameter <code<semanticBlockLine</code> cannot be null
+	 * - The block must be in a terminated state
 	 * @param semanticBlockLine  Contains the field specifications
 	 * @return A block based on the field specifications in semanticBlockLine
 	 */
@@ -45,12 +49,31 @@ class Block {    StringBuffer text = new StringBuffer();
 		return this; 
 	}
 
-	static Block empty() { return new Block();}
+	/**
+	 * Create an empty (no text and semantic information block) 
+	 * 
+	 * @return An empty block. 
+	 */
+    static Block empty() { return new Block();}
 	
-	boolean isEmpty() {  
+	/**
+	 * True if the block is empty, i.e. contains no semantic information and text 
+	 *
+	 * @return True if empty
+	 */
+    boolean isEmpty() {  
 		return fieldSpecs.isEmpty() && (text.length() == 0);
 	}
 	
+    /**
+     * Appends markdown text to this block.
+     * 
+     * Precondition:
+     * - The block is not terminated.
+     * 
+     * @param textValueLine The markdown line to be appended
+     * @return This block
+     */
 	Block appendText(CharSequence textValueLine) {
 		checkState(!terminated);
 		
@@ -59,18 +82,29 @@ class Block {    StringBuffer text = new StringBuffer();
 		return this;
 	}
 	
+	/**
+	 * A function used in reading the markdown to kept track of the block state. 
+	 * 
+	 * @return A terminated block or an empty block. 
+	 */
 	static Function <String, Block> block() {
 		Block block = new Block(); 
 		return line -> { 
 			             if (line.isBlank()) { block.terminate(); return block;}  
-		                 //else if (line.contains("{{") && line.contains("}}")) {block.semantics(line); return block;}
 		                 else if (line.contains("{{") && line.contains("}}")) {block.initialise(line); return block;}
                          else if (!block.isTerminated()) {block.appendText(line); return Block.empty();}
                          else {return Block.empty();}  // Line has text that does not have an associated semantic block
 		               };
 	}
 	
+	
+	/**
+	 * Converts the information in this block to a value map
+	 * 
+	 * @return A value map representing the semantic information in this block.
+	 */
 	ValueMap toValueMap() {
+	 
         ValueMap valueMap = new ValueMap();
 		
 		for (FieldSpec fieldSpec: fieldSpecs) {
@@ -93,6 +127,11 @@ class Block {    StringBuffer text = new StringBuffer();
 		
 	}
 	
+	/**
+	 * Returns an array of delimiter objects representing the delimiters specified in this block. 
+	 * 
+	 * @return Array of delimiters
+	 */
 	Delimiter[] delimiters() {
 		Delimiter[] delimiters = new Delimiter[fieldSpecs.size()];
 		
@@ -102,16 +141,28 @@ class Block {    StringBuffer text = new StringBuffer();
 		return delimiters;
 	}
 	
+	/**
+	 * Signals that the block is terminated, i.e. the semantic information and text for this 
+	 * block has been fully entered.
+	 * @return This block
+	 */
 	Block terminate() {
 		terminated = true;
 
 		return this;
 	}
 	
+	/**
+	 * Has the block been terminated, i.e. has the semantic information and text for this 
+	 * block has been fully entered.
+	 * 
+	 * @return True if terminated 
+	 */
 	boolean isTerminated() {
 		return terminated;
 	}
 		 
+	
 	@Override
 	public String toString() {
 		return "Block [fieldSpecs=" + fieldSpecs + ", text=" + text + "]";
